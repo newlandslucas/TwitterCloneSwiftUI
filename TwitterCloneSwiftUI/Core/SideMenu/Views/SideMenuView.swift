5bb83c10a2a7f660aae278705a443933355661b8
newlandslucas <lucasnewlands.m@gmail.com>
2022-04-24 18:49:22 -0300

    feat: SideMenuView finished

--- TwitterCloneSwiftUI/Core/SideMenu/Views/SideMenuView.swift
+++ TwitterCloneSwiftUI/Core/SideMenu/Views/SideMenuView.swift
@@ -9,23 +9,52 @@ import SwiftUI
 
 struct SideMenuView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Circle()
-                .frame(width: 48, height: 48)
-            
-            VStack(alignment: .leading, spacing: 4) {
-                Text("Lucas Newlands")
-                    .font(.headline)
+        VStack(alignment: .leading, spacing: 32) {
+            VStack(alignment: .leading) {
+                Circle()
+                    .frame(width: 48, height: 48)
                 
-                Text("newlandsLM")
-                    .font(.caption)
-                    .foregroundColor(.gray)
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Lucas Newlands")
+                        .font(.headline)
+                        
+                    
+                    Text("newlandsLM")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+                
+                UserStateView()
+                    .padding(.vertical)
+               
+               
+            }
+            .padding(.leading)
+            ForEach(SideMenuViewModel.allCases, id: \.rawValue) { viewModel in
+                if viewModel == .profile {
+                    NavigationLink {
+                        ProfileView()
+                    } label: {
+                        SideMenuOptionRowView(viewModel: viewModel)
+                    }
+                    
+                } else if  viewModel == .logout {
+                    Button {
+                        print("Handle logout here...")
+                    } label: {
+                        SideMenuOptionRowView(viewModel: viewModel)
+                    }
+
+                } else {
+                    SideMenuOptionRowView(viewModel: viewModel)
+                }
+              
             }
             
-            UserStateView()
-                .padding(.vertical)
-           
+            Spacer()
         }
+       
+        
     }
 }
 
@@ -34,3 +63,5 @@ struct SideMenuView_Previews: PreviewProvider {
         SideMenuView()
     }
 }
+
+
