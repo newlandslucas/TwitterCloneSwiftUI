3e3172856fdd352dfb3df53046e15985209deb2b
newlandslucas <lucasnewlands.m@gmail.com>
2022-04-24 13:38:27 -0300

    Initial Commit

---
+++ TwitterCloneSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterCloneSwiftUI
+//
+//  Created by Lucas Newlands on 24/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterCloneSwiftUI/TwitterCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneSwiftUIApp.swift
+//  TwitterCloneSwiftUI
+//
+//  Created by Lucas Newlands on 24/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
