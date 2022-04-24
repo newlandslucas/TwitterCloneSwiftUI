cfdf92433a3a35c7b85256ab8b4dbc6bd5b471ab
newlandslucas <lucasnewlands.m@gmail.com>
2022-04-24 14:28:28 -0300

    feat: TweetRowView component

---
+++ TwitterCloneSwiftUI/Core/Components/Tweets/TweetRowView.swift
@@ -0,0 +1,93 @@
+//
+//  TweetRowView.swift
+//  TwitterCloneSwiftUI
+//
+//  Created by Lucas Newlands on 24/04/22.
+//
+
+import SwiftUI
+
+struct TweetRowView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            // profile image + user info
+            HStack(alignment: .top, spacing: 12) {
+                Circle()
+                    .frame(width: 56, height: 56)
+                    .foregroundColor(Color(.systemBlue))
+                
+                
+                // user info + tweet caption
+                VStack(alignment: .leading, spacing: 4) {
+                    
+                    //user info
+                    HStack {
+                        Text("Lucas Newlands")
+                            .font(.subheadline).bold()
+                        
+                        Text("@lucasnewlands_")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                        
+                        Text("1w")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                    }
+                    
+                    //tweet caption
+                    
+                    Text("Twitter Clone with Swift & SwiftUI")
+                        .font(.subheadline)
+                        .multilineTextAlignment(.leading)
+                }
+            }
+            //action buttons
+            
+            HStack {
+                Button {
+                    //action
+                } label: {
+                    Image(systemName: "bubble.left")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Button {
+                    //action
+                } label: {
+                    Image(systemName: "arrow.2.squarepath")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+
+                Button {
+                    //action
+                } label: {
+                    Image(systemName: "heart")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+
+                Button {
+                    //action
+                } label: {
+                    Image(systemName: "bookmark")
+                        .font(.subheadline)
+                }
+
+
+            }
+            .padding()
+        }
+        .padding()
+    }
+}
+
+struct TweetRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetRowView()
+    }
+}
