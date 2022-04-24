1e0bdbc126c28fb8409b4fc3403937ca09e12fad
newlandslucas <lucasnewlands.m@gmail.com>
2022-04-24 14:49:25 -0300

    feat: Tab navigation

---
+++ TwitterCloneSwiftUI/Core/Explore/ExploreView.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreView.swift
+//  TwitterCloneSwiftUI
+//
+//  Created by Lucas Newlands on 24/04/22.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text("Explore View")
+    }
+}
+
+struct ExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreView()
+    }
+}
---
+++ TwitterCloneSwiftUI/Core/Notifications/NotificationsView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  TwitterCloneSwiftUI
+//
+//  Created by Lucas Newlands on 24/04/22.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text("Novifications View")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationsView()
+    }
+}
