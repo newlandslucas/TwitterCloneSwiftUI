724b884b770692d1556a03e863398c18b0bb2e4f
newlandslucas <lucasnewlands.m@gmail.com>
2022-04-24 21:35:51 -0300

    feat: NewTweetView finished

---
+++ TwitterCloneSwiftUI/Components/TextArea.swift
@@ -0,0 +1,36 @@
+//
+//  TextArea.swift
+//  TwitterCloneSwiftUI
+//
+//  Created by Lucas Newlands on 24/04/22.
+//
+
+import SwiftUI
+
+struct TextArea: View {
+    @Binding var text: String
+    let placeHolder: String
+    
+    init(_ placeholder: String, text: Binding<String>) {
+        self.placeHolder = placeholder
+        self._text = text
+        UITextView.appearance().backgroundColor = .clear
+    }
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            
+            if text.isEmpty {
+                Text(placeHolder)
+                    .foregroundColor(Color(.placeholderText))
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 12)
+            }
+            
+            TextEditor(text: $text)
+                .padding(4)
+        }
+        .font(.body)
+    }
+}
+
+
